945750d75854e559058e95109148a3af564da3d3
dannycahyo <dannydwicahyono@gmail.com>
2023-03-23 13:52:08 +0700

    feat: adjust open ai prompt

--- src/pages/api/jsontots/prompt.ts
+++ src/pages/api/jsontots/prompt.ts
@@ -1,15 +1,13 @@
 function jsonToTsPrompt(jsonData: string): string {
   return `
   I want you to convert JSON into TypeScript typing. For example:
-  From:
+  
   {
     "userId": "1",
     "age": 20,
     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit "
   }
-
-  To:
 
   type User = {
     userId: string;
